b037b5a86e65339163fb8c08c205d2be70a8ebd8
verzogern <tone121@cox.net>
2017-05-22 21:59:45 -0700

    Added Mesh Class
    Purpose: A General-Purpose class for primitive rendering.

---
+++ include/graphics/Mesh.h
@@ -0,0 +1,32 @@
+#ifndef AQUAGL_MESH_H
+#define AQUAGL_MESH_H
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+
+struct Vertex {
+	Vertex(float x, float y, float z) { position = glm::vec3(x, y, z); }
+	glm::vec3 position;
+};
+
+enum buffer_pos {
+	POSITION_POS = 0
+};
+
+class Mesh{
+private:
+	static const unsigned int NUM_BUFFERS = 1;
+
+	GLsizei DrawCount;
+
+	GLuint m_VertexArrayObject;
+	GLuint m_VertexArrayBuffers[NUM_BUFFERS];
+public:
+	Mesh(Vertex* vertices, GLuint VertexCount);
+	~Mesh();
+
+	void Draw();
+};
+
+#endif
+
---
+++ src/graphics/Mesh.cpp
@@ -0,0 +1,30 @@
+#include <graphics/Mesh.h>
+
+Mesh::Mesh(Vertex* vertices, GLsizei VertexCount) : DrawCount(VertexCount)
+{
+	glGenVertexArrays(1, &m_VertexArrayObject);
+	glBindVertexArray(m_VertexArrayObject); // bind vao
+
+	glGenBuffers(NUM_BUFFERS, m_VertexArrayBuffers);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VertexArrayBuffers[POSITION_POS]);
+	glBufferData(GL_ARRAY_BUFFER, VertexCount * sizeof(vertices[0]), vertices, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0); // layout 0
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0)
+
+	glBindVertexArray(0); // unbind vao
+}
+
+Mesh::~Mesh()
+{
+	glDeleteVertexArrays(1, &m_VertexArrayObject);
+	glDeleteBuffers(NUM_BUFFERS, &m_VertexArrayBuffers);
+}
+
+void Draw()
+{
+	glBindVertexArray(m_vertexArrayObject);
+	glDrawArrays(GL_TRIANGLES, 0, DrawCount);
+	glBindVertexArray(0);
+}
+

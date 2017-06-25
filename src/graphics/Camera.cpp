5f7e22cde35fdf20172e31075ded615acd2daab9
verzogern <tone121@cox.net>
2017-06-25 01:25:37 -0700

    Tweaked some camera maths.

--- src/graphics/Camera.cpp
+++ src/graphics/Camera.cpp
@@ -11,7 +11,7 @@ Camera::Camera(const glm::vec3& pos, float fov, float aspect, float zNear, float
 void Camera::Update(float yaw, float pitch, const glm::vec3& pos, float fov, float aspect, float zNear, float zFar)
 {
         SetView(yaw, pitch);
-        m_Projection = glm::perspective(fov, aspect, zNear, zFar);
+        m_Projection = glm::perspective(glm::radians(fov), aspect, zNear, zFar);
         m_pos = pos;
         m_up = glm::vec3(0, 1, 0);
 }
